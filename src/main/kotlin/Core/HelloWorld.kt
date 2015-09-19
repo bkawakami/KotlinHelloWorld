22fec95eb569c7da4826a206762f4c40f44f06f0
Bruno Kawakami <bru.kawakami@gmail.com>
2015-09-19 00:49:26 -0300

    First compilable project :)

---
+++ src/main/kotlin/Core/HelloWorld.kt
@@ -0,0 +1,10 @@
+package kotlin.Core
+
+/**
+ * Created by bruno on 9/18/15.
+ */
+public object HelloWorld {
+    public fun printHelloWorld () {
+        println("Hello World")
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+package kotlin
+
+import kotlin.Core.HelloWorld
+
+/**
+ * Created by bruno on 9/18/15.
+ */
+public object Main {
+    JvmStatic fun main(args: Array<String>){
+        HelloWorld.printHelloWorld()
+    }
+}
